0cf7af4ac064b7e593d7d08854ccc53dc1b81afb
José de Souza <josee.souzaaa@gmail.com>
2021-12-09 20:27:40 -0300

    Services att

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const tmdb = axios.create({
-  baseURL: 'https://api.themoviedb.org/3'
+  baseURL: process.env.URL_TMDB
 })
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: process.env.URL_APLICATION
 })
